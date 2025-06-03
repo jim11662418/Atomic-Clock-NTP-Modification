edf326ff1eb23a31f510ef115dde181c8e7c87c2
Jim <51113579+jim11662418@users.noreply.github.com>
2025-06-03 02:52:26 -0400

    Update AtomicClock.ino

--- AtomicClock.ino
+++ AtomicClock.ino
@@ -1,4 +1,19 @@
-//--------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------------------------
+// Copyright 2025 Jim Loos
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+// (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sub-license, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+// IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//-------------------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------------------------
 // This sketch uses an ESP8266 module to replace the radio reciever in an 'Atomic" clock.
 // The sketch:
 //   1. connects to WiFi
@@ -6,7 +21,7 @@
 //   3. sends a digital signal to the clock controller that makes the controller think
 //      that it's receiving the WWVB time signal from Ft. Collins CO.
 //   4. flashes an LED on the face of the clock just because.
-//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------------
 
 #include <ESP8266WiFi.h>   
 #include <Ticker.h>    
